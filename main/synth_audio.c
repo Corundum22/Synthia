16bd72ad23b09a67c2884bf0afcd47ebbd1f0382
Corundum22 <jeisbrenn@gmail.com>
2025-03-08 12:03:29 -0700

    Continuous synchronous dac implemented. Functional at low frequencies. Performance at high frequencies is expected to be good but is unknown due to lower end test equipment. Currently testing with square wave. Poor comaptibility with currently envelope control must be fixed.

--- main/synth_audio.c
+++ main/synth_audio.c
@@ -10,6 +10,7 @@
 #include "note_handler.h"
 #include "global_header.h"
 #include "sequencer.h"
+#include <string.h>
 
 
 #define NUM_COMB_VOICES (NUM_VOICES + SEQ_VOICES)
@@ -17,30 +18,30 @@
 #define GET_NOTE(i) \
 if (note_properties[i].is_sounding == true) {\
     data += wave(note_properties[i].note_num, note_properties[i].multiplier, time);\
-    times_added++;\
 }
 
 
 
 dac_continuous_handle_t dac_handle;
-QueueHandle_t dac_event_queue;
 
 
-static inline int_fast32_t wave(uint_fast8_t midi_note_number, uint_fast16_t multiply_val, uint_fast32_t time) {
+static inline uint16_t wave(uint_fast8_t midi_note_number, uint_fast16_t multiply_val, uint_fast32_t time) {
     uint_fast32_t ratio_numerator = ratio_num[midi_note_number];
     uint_fast32_t ratio_denominator = ratio_denom[midi_note_number];
 
     uint_fast32_t point_in_cycle = ((time * ratio_numerator) / ratio_denominator) & 0b11111111;
+    //uint_fast32_t point_in_cycle = ((time / 21) & 0xff);
 
-    int_fast32_t result = (int_fast32_t) sin_array[point_in_cycle];
-    result = (result * multiply_val) >> MULTIPLIER_WIDTH;
+    //int_fast32_t result = (int_fast32_t) sin_array[point_in_cycle];
+    //result = (result * multiply_val) >> MULTIPLIER_WIDTH;
+    //uint16_t result = sin_array[(time) & 0b11111111];
+    uint16_t result = (point_in_cycle < 128) ? 0xffff : 0;
 
     return result;
 }
 
 static inline uint16_t audio_sample_get(uint32_t time) {
-    int16_t data = 0;
-    uint_fast8_t times_added = 0;
+    uint16_t data = 0;
 
     #if NUM_COMB_VOICES >= 1
         GET_NOTE(0)
@@ -74,17 +75,20 @@ static inline uint16_t audio_sample_get(uint32_t time) {
     #endif
 
     // TODO: uncomment to prevent clipping with multiple voices active
-    //if (times_added != 0) data /= (NUM_VOICES + SEQ_VOICES);
+    //data /= (NUM_VOICES + SEQ_VOICES);
     
-    uint16_t unsigned_data = ((uint16_t) data) + 0b0111111111111111;
+    //uint16_t unsigned_data = ((uint16_t) data) + 0b0111111111111111;
 
-    return unsigned_data;
+    return data;
+    //return unsigned_data;
     //ESP_ERROR_CHECK(dac_oneshot_output_voltage(lower_dac_handle, (uint8_t) unsigned_data));
     //ESP_ERROR_CHECK(dac_oneshot_output_voltage(upper_dac_handle, (uint8_t) (unsigned_data >> 8)));
 }
 
 #define AUDIO_CHUNK_SIZE 256
 #define NUM_DAC_CHANNELS 2
+
+static uint8_t data_array[AUDIO_BUF_SIZE * NUM_DAC_CHANNELS];
 void task_audio_generate() {
 
     uint32_t time = 0;
@@ -92,48 +96,31 @@ void task_audio_generate() {
     while (1) {
 
         dac_event_data_t event_data;
-        size_t byte_written = 0;
-        while (byte_written < AUDIO_CHUNK_SIZE) {
-            xQueueReceive(dac_event_queue, &event_data, portMAX_DELAY);
-
  
-            uint8_t data_array[AUDIO_CHUNK_SIZE * NUM_DAC_CHANNELS];
-            for (uint_fast16_t i = 0; i < AUDIO_CHUNK_SIZE; i++) {
-                uint16_t data = audio_sample_get(time);
-                
-                data_array[i * 2] = (uint8_t) data;
-                data_array[(i * 2) + 1] = (uint8_t) (data >> 8);
+        for (uint_fast16_t i = 0; i < AUDIO_BUF_SIZE; i++) {
+            uint16_t data = audio_sample_get(time);
             
-                time++;
-            }
-
-
-            size_t loaded_bytes = 0;
-            ESP_ERROR_CHECK(dac_continuous_write_asynchronously(dac_handle, event_data.buf, event_data.buf_size, data_array, AUDIO_CHUNK_SIZE * NUM_DAC_CHANNELS, &loaded_bytes));
-            
-            byte_written += loaded_bytes;
-        }
-    
-        while (uxQueueSpacesAvailable(dac_event_queue) == 0) {
-            vTaskDelay(pdMS_TO_TICKS(2));
+            data_array[i * 2] = (uint8_t) data;
+            data_array[(i * 2) + 1] = (uint8_t) (data >> 8);
+        
+            time++;
         }
 
+
+        ESP_ERROR_CHECK(dac_continuous_write(dac_handle, data_array, AUDIO_BUF_SIZE * NUM_DAC_CHANNELS, NULL, -1));
+   
+
+        /*while (uxQueueSpacesAvailable(dac_event_queue) == 0) {
+            vTaskDelay(pdMS_TO_TICKS(1));
+        }*/
+
+        /*for (int i = 0; i < 4; i++) {
+            xQueueReceive(dac_event_queue, &event_data, portMAX_DELAY);
+            memset(event_data.buf, 0, event_data.buf_size);
+        }*/
+        vTaskDelay(pdMS_TO_TICKS(1));
+
     }
-}
-
-static bool IRAM_ATTR dac_on_convert_done_callback(dac_continuous_handle_t handle, const dac_event_data_t *event, void *user_data) {
-    QueueHandle_t user_data_queue = (QueueHandle_t) user_data;
-    BaseType_t need_awoke;
-
-    // Drop oldest item, when queue is full
-    if (xQueueIsQueueFullFromISR(user_data_queue)) {
-        dac_event_data_t dummy_data;
-        xQueueReceiveFromISR(user_data_queue, &dummy_data, &need_awoke);
-    }
-
-    // Send the event from callback
-    xQueueSendFromISR(user_data_queue, event, &need_awoke);
-    return need_awoke;
 }
 
 void dac_init() {
@@ -145,21 +132,10 @@ void dac_init() {
         .freq_hz = OUTPUT_SAMPLE_RATE,
         .offset = 0, // TODO: use this instead of hardcoding an offset
         .clk_src = DAC_DIGI_CLK_SRC_APLL,
-        .chan_mode = DAC_CHANNEL_MODE_SIMUL,
+        .chan_mode = DAC_CHANNEL_MODE_ALTER,
     };
     ESP_ERROR_CHECK(dac_continuous_new_channels(&dac_config, &dac_handle));
 
 
-    dac_event_queue = xQueueCreate(10, sizeof(dac_event_data_t));
-    dac_event_callbacks_t cbs = {
-        .on_convert_done = dac_on_convert_done_callback,
-        .on_stop = NULL,
-    };
-    ESP_ERROR_CHECK(dac_continuous_register_event_callback(dac_handle, &cbs, dac_event_queue));
-
-
     ESP_ERROR_CHECK(dac_continuous_enable(dac_handle));
-    
-
-    ESP_ERROR_CHECK(dac_continuous_start_async_writing(dac_handle));
 }
